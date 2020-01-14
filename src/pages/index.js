bb69ce383d7af1c3cd9903c7580f815134875eb1
nubelsondev <nubelsondev@hotmail.com>
2020-01-14 23:01:28 +0000

    feat(component): add Board

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import React from "react"
 import GlobalStyle from "../styles/global"
 
 import Header from "../components/Header"
+import Board from "../components/Board"
 
 const App = () => {
     return (
         <>
             <GlobalStyle />
             <Header />
+            <Board />
         </>
     )
 }

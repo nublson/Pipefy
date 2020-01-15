34f1e9e7636c5e18e81ad3ed690bf76e7a725d5e
nubelsondev <nubelsondev@hotmail.com>
2020-01-15 16:01:20 +0000

    fix(head): add title

---
+++ src/components/Head.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+const Head = () => {
+    return (
+        <Helmet>
+            <title>Pipefy</title>
+        </Helmet>
+    )
+}
+
+export default Head
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import HTML5Backend from "react-dnd-html5-backend"
 
 import GlobalStyle from "../styles/global"
 
+import Head from "../components/Head"
 import Header from "../components/Header"
 import Board from "../components/Board"
 
 const App = () => {
     return (
         <DndProvider backend={HTML5Backend}>
+            <Head />
             <GlobalStyle />
             <Header />
             <Board />

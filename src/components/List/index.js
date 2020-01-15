3d181292eb139ea5386959f988acbd6c95e07642
nubelsondev <nubelsondev@hotmail.com>
2020-01-15 07:49:21 +0000

    fix(style): add opacity to done List

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -7,7 +7,7 @@ import { Container } from "./styles"
 
 const List = ({ data }) => {
     return (
-        <Container>
+        <Container done={data.done}>
             <header>
                 <h2>{data.title}</h2>
                 {data.creatable && (
--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
     padding: 0 1.5rem;
     height: 100%;
     flex: 0 0 32rem;
+    opacity: ${props => (props.done ? 0.6 : 1)};
 
     & + div {
         border-left: 1px solid rgba(0, 0, 0, 0.05);
